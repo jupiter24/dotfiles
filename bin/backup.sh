bbe359d3164aa6504e5acbdbbafd117759395b2a
Erik Jenner <erik.jenner99@gmail.com>
2021-04-08 17:10:45 +0200

    Backup from home directory

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd
 # p, -o and -g don't work well with FAT32, therefore -a shouldn't be used
 # --modify-windows is needed because FAT32 handles timestamps differently
-rsync --itemize-changes -rltDR --modify-window=1 --include-from=$HOME/.backup.txt . /run/media/erik/INTENSO/earth_backup/
+rsync --itemize-changes -rltDR --modify-window=1 --include-from=.backup.txt . /run/media/erik/INTENSO/earth_backup/
+cd -
--- bin/usb_backup.sh
+++ bin/usb_backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cd
 # -p, -o and -g don't work well with FAT32, therefore -a shouldn't be used
 # --modify-windows is needed because FAT32 handles timestamps differently
-rsync --itemize-changes -rltDR --modify-window=1 --include-from=$HOME/.backup_small.txt . /run/media/erik/ERIK/backup/
+rsync --itemize-changes -rltDR --modify-window=1 --include-from=.backup_small.txt . /run/media/erik/ERIK/backup/
+cd -
