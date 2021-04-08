3bfbc983f8fb86e19b6c9bafb4912f282d3efde4
Erik Jenner <erik.jenner99@gmail.com>
2021-04-08 17:02:35 +0200

    Fix backup config path

--- bin/usb_backup.sh
+++ bin/usb_backup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 # -p, -o and -g don't work well with FAT32, therefore -a shouldn't be used
 # --modify-windows is needed because FAT32 handles timestamps differently
-rsync --itemize-changes -rltDR --modify-window=1 --include-from=.backup_small.txt . /run/media/erik/ERIK/backup/
+rsync --itemize-changes -rltDR --modify-window=1 --include-from=$HOME/.backup_small.txt . /run/media/erik/ERIK/backup/
